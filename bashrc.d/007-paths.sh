2bce19d10bcfd12b6814accd1ac1a2f68cfd9b35
jesse <jessedp@gmail.com>
2021-03-25 14:24:36 -0400

    actually append to PATH

--- bashrc.d/007-paths.sh
+++ bashrc.d/007-paths.sh
@@ -3,8 +3,8 @@ export GOROOT=/usr/lib/go-1.10
 append PATH $GOROOT/bin:$GOPATH/bin
 #append $GOROOT
 
-append $HOME/.config/composer/vendor/bin
-append $HOME/.local/bin
+append PATH $HOME/.config/composer/vendor/bin
+append PATH $HOME/.local/bin
 
 # RUST bins
 if [ -d $HOME/.cargo/bin ]; then
