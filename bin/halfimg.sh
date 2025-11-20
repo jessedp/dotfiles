69937b037fb0af07fb06de26e8ecb7d4c0d57343
jesse <jessedp@gmail.com>
2025-11-19 20:41:01 -0500

    change halfimg to save new file next to src

--- bin/halfimg.sh
+++ bin/halfimg.sh
@@ -3,12 +3,14 @@
 files=( "$@" )
 for arg;do
 	if [ -f "$arg" ]; then
+		dirname=$(dirname -- "$arg")
 		filename=$(basename -- "$arg")
 		extension="${filename##*.}"
 		filename="${filename%.*}"
+		output_file="$dirname/$filename.half.$extension"
 
-		echo creating "$filename.half.$extension"
-		convert -quality 50 -resize 50% "$arg" "$filename.half.$extension"
+		echo creating "$output_file"
+		convert -quality 50 -resize 50% "$arg" "$output_file"
 	else
 		echo "invalid file $arg"
 	fi
